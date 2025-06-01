70a2f4057573253421afdaa41341a61434ddb4f3
NguyenTienThanh22 <thanhnthe186221@fpt.edu.vn>
2025-06-01 23:20:58 +0700

    add patient dao and lombok to patient class

---
+++ src/java/DAO/PatientDao.java
@@ -0,0 +1,60 @@
+package Dao;
+
+import DAO.DBContext;
+import Model.Gender;
+import Model.Patient;
+import Model.Role;
+import Model.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PatientDao {
+    public static List<Patient> getAllPatients() {
+        List<Patient> patients = new ArrayList<>();
+        String sql = "SELECT p.*, u.*, r.name AS role_name, r.description AS role_description " +
+                "FROM patients p " +
+                "JOIN users u ON p.user_id = u.user_id " +
+                "JOIN roles r ON u.role_id = r.role_id";
+        try (Connection conn = DBContext.makeConnection();
+             PreparedStatement ps = conn.prepareStatement(sql);
+             ResultSet rs = ps.executeQuery()) {
+            while (rs.next()) {
+                patients.add(mappingPatient(rs));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return patients;
+    }
+    private static Patient mappingPatient(ResultSet rs) throws SQLException {
+        Patient patient = new Patient();
+        patient.setPatient_id(rs.getInt("patient_id"));
+        patient.setGender(Gender.valueOf(rs.getString("gender")));
+        patient.setDate_of_birth(rs.getDate("date_of_birth"));
+        patient.setAddress(rs.getString("address"));
+        patient.setImage_url(rs.getString("image_url"));
+        patient.setCreated_at(rs.getTimestamp("created_at"));
+
+        User user = new User();
+        user.setUser_id(rs.getInt("user_id"));
+        user.setUsername(rs.getString("username"));
+        user.setPassword(rs.getString("password"));
+        user.setFullname(rs.getString("full_name"));
+        user.setEmail(rs.getString("email"));
+        user.setPhone(rs.getString("phone"));
+        user.setCreated_at(rs.getTimestamp("users.created_at"));
+
+        Role role = new Role();
+        role.setName(rs.getString("role_name"));
+        role.setDescription(rs.getString("role_description"));
+        user.setRole(role);
+
+        patient.setUser(user);
+        return patient;
+    }
+}

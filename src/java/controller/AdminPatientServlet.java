524952752efa4bea6f70691e1544180ba79a55be
NguyenTienThanh22 <thanhnthe186221@fpt.edu.vn>
2025-06-06 22:26:50 +0700

    add doPost of AdminPatientServlet

--- src/java/controller/AdminPatientServlet.java
+++ src/java/controller/AdminPatientServlet.java
@@ -29,4 +29,22 @@ public class AdminPatientServlet extends HttpServlet{
         req.setAttribute("patients", patients);
         req.getRequestDispatcher("/views/admin/patient_manager.jsp").forward(req, resp);
     }
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+        String fullname = req.getParameter("fullname");
+        String email = req.getParameter("email");
+        String phone = req.getParameter("phone");
+        Role role = RoleDao.getRoleWithName("patient");
+        User user = new User(username, password, fullname, email, phone, role);
+        UserDAO.insertUser(user);
+        Gender gender = Gender.valueOf(req.getParameter("gender"));
+        Date date_of_birth = Date.valueOf(req.getParameter("date_of_birth"));
+        String address = req.getParameter("address");
+        String image_url = UploadImage.saveImage(req, "image");
+        Patient patient = new Patient(user, gender, date_of_birth, address, image_url);
+        PatientDao.createPatient(patient);
+        resp.sendRedirect(req.getContextPath() + "/admin/patient");
+    }
 }

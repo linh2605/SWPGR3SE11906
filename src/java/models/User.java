b7e5dabf19e0943c2e3e950372e87bf0ff0742c7
linhvqhe176018 <linhvqhe176018@fpt.edu.vn>
2025-06-09 08:31:43 +0700

    uplate
    uplate User

--- src/java/models/User.java
+++ src/java/models/User.java
@@ -11,6 +11,7 @@ import java.sql.Timestamp;
 @ToString
 public class User {
     private int user_id;
+    private int RoleID;
     private String username;
     private String password;
     private String fullname;
@@ -18,6 +19,8 @@ public class User {
     private String phone;
     private Role role;
     private Timestamp created_at;
+
+    // Constructing with role
     public User(String username, String password, String fullname, String email, String phone, Role role) {
         this.username = username;
         this.password = password;
@@ -25,5 +28,26 @@ public class User {
         this.email = email;
         this.phone = phone;
         this.role = role;
+    }
+    public User(String username, String password, int user_id, String fullname, String email, String phone, Role role, String email1, Timestamp created_at) {
+        this.user_id = user_id;
+        this.username = username;
+        this.password = password;
+        this.fullname = fullname;
+        this.email = email;
+        this.phone = phone;
+        this.role = role;
+        this.created_at = created_at;
+    }
+    
+    
+    // Setter for user_id
+    public void setId(int user_id) {
+        this.user_id = user_id;
+    }
+    
+    // Setter for RoleID
+    public void setRoleID(int roleID) {
+        this.RoleID = roleID;
     }
 }

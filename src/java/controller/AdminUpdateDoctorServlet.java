0cdc0f76071598fb4c659c2955814510aabd9365
PhamHienTam <tamphhe182476@fpt.edu.vn>
2025-06-20 20:37:05 +0700

    update AdminUpdateDoctorServlet

--- src/java/controller/AdminUpdateDoctorServlet.java
+++ src/java/controller/AdminUpdateDoctorServlet.java
@@ -2,9 +2,8 @@ package controller;
 
 import Util.UploadImage;
 import dal.DoctorDao;
-import dal.RoleDao;
-import dal.UserDAO;
 import dal.SpecialtyDao;
+import dal.UserDAO;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.MultipartConfig;
 import jakarta.servlet.annotation.WebServlet;
@@ -23,38 +22,58 @@ public class AdminUpdateDoctorServlet extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         int doctor_id = Integer.parseInt(req.getParameter("doctor_id"));
         Doctor doctor = DoctorDao.getDoctorById(doctor_id);
-        assert doctor != null;
         User user = doctor.getUser();
+        int user_id = user.getUserId();
+
         String username = req.getParameter("username");
-        String password = req.getParameter("password");
-        String fullName = req.getParameter("fullname");
         String email = req.getParameter("email");
+
+        if (UserDAO.doesUsernameExistExcept(user_id, username)) {
+            req.getSession().setAttribute("error", "Username đang được sử dụng bởi user khác.");
+            resp.sendRedirect(req.getContextPath() + "/admin/doctor");
+            return;
+        }
+
+        if (UserDAO.doesEmailExistExcept(user_id, email)) {
+            req.getSession().setAttribute("error", "Email đang được sử dụng bởi user khác.");
+            resp.sendRedirect(req.getContextPath() + "/admin/doctor");
+            return;
+        }
+
+        String password = req.getParameter("password");
+        String fullname = req.getParameter("fullname");
         String phone = req.getParameter("phone");
+
         user.setUsername(username);
         user.setPassword(password);
-        user.setFullName(fullName);
+        user.setFullName(fullname);
         user.setEmail(email);
         user.setPhone(phone);
-        user.setRole(RoleDao.getRoleWithName("doctor"));
-        UserDAO.updateUser(doctor.getUser());
+        UserDAO.updateUser(user);
+
         Gender gender = Gender.valueOf(req.getParameter("gender"));
         Date dob = Date.valueOf(req.getParameter("dob"));
-        int specialtyId = Integer.parseInt(req.getParameter("specialty_id"));
-        Specialty specialty = SpecialtyDao.getSpecialtyById(specialtyId);
+        Specialty specialty = SpecialtyDao.getSpecialtyById(Integer.parseInt(req.getParameter("specialty_id")));
         String degree = req.getParameter("degree");
         String experience = req.getParameter("experience");
         Status status = Status.valueOf(req.getParameter("status"));
+
         doctor.setGender(gender);
         doctor.setDob(dob);
         doctor.setSpecialty(specialty);
         doctor.setDegree(degree);
         doctor.setExperience(experience);
         doctor.setStatus(status);
+
         if (req.getPart("image") != null && req.getPart("image").getSize() > 0) {
             String image_url = UploadImage.saveImage(req, "image");
             doctor.setImage_url(image_url);
         }
+
         DoctorDao.updateDoctor(doctor);
+
+        req.getSession().setAttribute("success", "Cập nhật bác sĩ thành công.");
         resp.sendRedirect(req.getContextPath() + "/admin/doctor");
     }
+
 }
